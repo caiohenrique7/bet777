fb4a795db6a02e744739f45aa898df2e15df2d94
Caio Henrique <caiohenriquee.dev@gmail.com>
2026-07-21 16:20:43 -0300

    implementando mais funcionalidades no float menu

--- assets/js/floatMenu.js
+++ assets/js/floatMenu.js
@@ -1,26 +1,47 @@
 const floatAposta = document.querySelector('.floatAposta');
+const selecoesFloat = document.querySelector('#selecoesFloat')
 const escolhaFloat = document.querySelector('#escolhasFloat');
 const oddFloat = document.querySelector('#oddFloat');
-
+let apostas = [];
 const opcoes = document.querySelectorAll(".opc-aposta");
-
 
 console.log(opcoes);
 
-
-
 opcoes.forEach(opcao => {
     opcao.addEventListener('click', () => {
-        let nomeTime = opcao.querySelector('.nomeTime');
-        let oddTime = opcao.querySelector('.oddTime');
+        let nomesAposta = opcao.querySelector('.nomeTime').textContent;
+        let novaOdd = Number(opcao.querySelector('.oddTime').textContent);
+        let aposta = 
+        {   nome: nomesAposta,
+            odd: novaOdd 
+        };
+        
+        apostas.push(aposta);
+        
+        console.log(apostas);
+        
+        let oddTotal = 1;
+        let nomes = [];
 
+        for(let i = 0; i < apostas.length; i++) {
+            oddTotal = oddTotal * apostas[i].odd;
+            nomes.push(apostas[i].nome);
+        }
+        
+        let qntdSelecoes = apostas.length;
 
+        if (qntdSelecoes == 1) {
+            selecoesFloat.innerHTML = qntdSelecoes + " Seleção"
+        } else {
+        selecoesFloat.innerHTML = qntdSelecoes + " Seleções"
+        }
+        
         floatAposta.style.display = 'flex'
-        escolhaFloat.innerHTML = nomeTime.textContent
-        oddFloat.innerHTML = oddTime.textContent
-        opcao.style.background = '#1E88FF'
+        escolhaFloat.innerHTML = nomes
+        oddFloat.innerHTML = oddTotal.toFixed(2);
+        opcao.style.background = '#1E88FF' 
 
     });
 });
 
-
+console.log(apostas);
